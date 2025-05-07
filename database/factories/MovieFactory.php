172ee3c46a272b6f20ee8444321eff2f21d820bd
haurazhr <04hauraola@gmail.com>
2025-05-07 16:01:50 +0700

    Seeder and Factory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -3,21 +3,29 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Category;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
- */
 class MovieFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
+        $title = fake()->sentence(rand(3, 6));
+        $slug = Str::slug($title);
+
         return [
-            //
+            'title'       => $title,
+            'slug'        => $slug,
+            'synopsis'    => fake()->paragraph(rand(5, 10)),
+            'category_id' => Category::inRandomOrder()->first(),
+            'year'        => fake()->year(),
+            'actors'      => fake()->name() . ', ' . fake('id')->name().','.fake()->name(),
+            'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640',
+            'created_at'  => now(),
+            'updated_at'  => now(),
         ];
     }
 }
